d75a36fb5218a8b25a5b3e6d8eb5cd31ecf284e0
Andrei Timotin <andreitimotin11@gmail.com>
2016-11-08 16:55:39 +0200

    add function getArray

--- www/index.php
+++ www/index.php
@@ -1,6 +1,33 @@
 <?php
 /*
  * Functions and arrays
+ *
  */
-$a = [1,2,3,4,5];
-$b = 12;
+
+$i = 1;
+while($i <= 100){
+	if($i % 3 == 0){
+		echo $i . "<br>";
+	}
+	$i++;
+}
+/* Cream masiv din n elemente aleatorii si le
+*  sortam in ordine descrescatoare
+*/
+function getArray(){
+	$n = rand(1, 100);
+	for($i=0; $i < $n ; $i++){
+		$arr[$i] = rand(0, 1000);
+	}
+	return $arr;
+}
+$arr = getArray();
+//var_dump($arr);
+arsort($arr);
+$q = 0;
+echo "<h2>";
+foreach ($arr as $v){
+
+	if( $q++ % 5 == 0)echo "<br>";
+	echo $v . ", ";
+}
